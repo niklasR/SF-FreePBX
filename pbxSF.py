c5be380bee670dcf72dc08be309a80fb49ae050a
Niklas Rahmel <niklas@brandwatch.com>
2014-08-26 11:10:02 +0100

    Removed auto adding of .epk to -f argument to allow tabbing

--- pbxSF.py
+++ pbxSF.py
@@ -1446,7 +1446,7 @@ if __name__ == "__main__":
 		elif opt in ("-p", "--port"):
 			port = arg
 		elif opt in ("-f", "--configfile"):
-			configfile = arg + '.epk'
+			configfile = arg
 			filesecret = getpass.getpass("Enter password for " + arg)
 	
 	exitnow = False
